4ca6684fd463b44ff1de159389a881bb2d4d79b5
Sara Samreen <sarasamreen73@gmail.com>
2026-07-02 17:04:25 +0500

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Mac on 02/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
